d051ec101b7fe6025eab506739e309b89c966f97
Deployment Manager <sysadmin@firestitch.com>
2024-10-05 12:05:21 +0000

    Angular 17 Upgrade

--- src/app/modules/one-time-passwords/components/one-time-password-dialog/one-time-password-dialog.component.ts
+++ src/app/modules/one-time-passwords/components/one-time-password-dialog/one-time-password-dialog.component.ts
@@ -62,7 +62,7 @@ export class OneTimePasswordDialogComponent implements OnInit, OnDestroy {
   };
 
   public ngOnDestroy(): void {
-    this._destroy$.next();
+    this._destroy$.next(null);
     this._destroy$.complete();
   }
 
--- src/app/modules/one-time-passwords/components/one-time-passwords/one-time-passwords.component.ts
+++ src/app/modules/one-time-passwords/components/one-time-passwords/one-time-passwords.component.ts
@@ -78,7 +78,7 @@ export class OneTimePasswordsComponent implements OnInit, OnDestroy {
   }
 
   public ngOnDestroy(): void {
-    this._destroy$.next();
+    this._destroy$.next(null);
     this._destroy$.complete();
   }
 
